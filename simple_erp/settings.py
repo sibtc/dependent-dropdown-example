357c369aa652719268a20952b36c88a6e5be4b35
Vitor Freitas <vitorfs@gmail.com>
2018-01-29 00:37:37 +0200

    Add STATIC_ROOT

--- simple_erp/settings.py
+++ simple_erp/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -122,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
