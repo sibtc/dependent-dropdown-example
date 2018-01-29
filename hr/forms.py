7fb0ffffacdb76bbbfe6d5c8fefc740d2e35c435
Vitor Freitas <vitorfs@gmail.com>
2018-01-29 09:00:14 +0200

    Missing parenthesis

--- hr/forms.py
+++ hr/forms.py
@@ -15,7 +15,7 @@ class PersonForm(forms.ModelForm):
             try:
                 country_id = int(data.get('country'))
                 self.fields['city'].queryset = City.objects.filter(country_id=country_id).order_by('name')
-            except ValueError, TypeError:
+            except (ValueError, TypeError):
                 pass  # invalid input from the client; ignore and fallback to empty City queryset
         elif self.instance.pk:
             self.fields['city'].queryset = self.instance.country.city_set.order_by('name')
